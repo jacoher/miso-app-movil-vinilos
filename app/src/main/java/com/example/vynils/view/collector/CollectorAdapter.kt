e40d3b7f43f77d2505429c95e64d0e1bc9cf5c74
Julie Padilla <j.padillag@uniandes.edu.co>
2022-11-25 09:25:33 -0500

    Feature HU05 - Ver lista de coleccionistas terminada - faltan pruebas (#76)

---
+++ app/src/main/java/com/example/vynils/view/collector/CollectorAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.vynils.view.collector
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vynils.R
+import com.example.vynils.databinding.CollectorRowBinding
+import com.example.vynils.model.Collector
+
+class CollectorAdapter: RecyclerView.Adapter<CollectorAdapter.CollectorViewHolder>(){
+
+    var collectors :List<Collector> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectorViewHolder {
+        val withDataBinding: CollectorRowBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            CollectorViewHolder.LAYOUT,
+            parent,
+            false)
+        return CollectorAdapter.CollectorViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: CollectorViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.collector = collectors[position]
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return collectors.size
+    }
+
+    class CollectorViewHolder(val viewDataBinding: CollectorRowBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.collector_row
+        }
+    }
+}
